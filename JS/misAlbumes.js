bd0f0d0bc6e67d9a9865a19bfbc3cc6628835e7a
RalowiecF <143044384+RalowiecF@users.noreply.github.com>
2023-11-06 15:50:19 -0300

    branchFede
    js misAlbums

---
+++ JS/misAlbumes.js
@@ -0,0 +1,44 @@
+//const albums = [
+//    { id: 1: }
+//]
+
+if(USER.albumsFav.length != 0){  
+    let albums = USER.albumsFav;
+    albums.forEach(element => {
+        let star = document.getElementById(element);
+        let articleElement = null;
+        if(star){
+            articleElement  = star.closest('article');
+        }
+        if(articleElement){
+            articleElement.classList.remove("oculto")
+        }
+    });
+}else{
+    showEmptyList()
+}
+
+document.removeEventListener('click', addAndRemoveStar)
+document.addEventListener('click', (event) => {
+
+    //AL HACER CLICK EN LA ESTRELLA SE AGREGA AL ARRAY DE FAVORITOS
+
+
+    if (document.getElementById(event.target.id)) {
+    var star = document.getElementById(event.target.id);
+           const article = star.closest('article')
+           if(article){
+              article.classList.add('oculto');
+           }
+           removeFavoriteAlbum(event.target.id)
+           if(USER.albumsFav.length == 0){
+            showEmptyList()
+           }
+        }
+     
+});
+
+function showEmptyList(){
+    const mensajeSinAlbumes = document.getElementById("sinAlbums");
+    mensajeSinAlbumes.classList.remove("oculto");
+}
--- JS/vistapantallaprincipal.js
+++ JS/vistapantallaprincipal.js
@@ -3,12 +3,13 @@ const favoritos = [];
 const USER = JSON.parse(localStorage.getItem("session"));
 const USUARIOS_REGISTRADOS = JSON.parse(localStorage.getItem("usuariosRegistrados"));
 
-if(USER.albumsFav.length != 0){
+if(USER.albumsFav.length != 0){  
     let albums = USER.albumsFav;
     albums.forEach(element => {
         let star = document.getElementById(element);
         if(star != null){
          star.classList.add('fa-solid');
+         star.classList.add('to-show')
         }
     });
 }
@@ -29,28 +30,28 @@ document.addEventListener('mouseover', (event) => {
      }
 });
 
-document.addEventListener('click', (event) => {
+document.addEventListener('click', addAndRemoveStar);
 
-    //AL HACER CLICK EN LA ESTRELLA SE AGREGA AL ARRAY DE FAVORITOS
+function addAndRemoveStar(event){     
+   if (document.getElementById(event.target.id)) {
+   var star = document.getElementById(event.target.id);
+   if(star.classList.contains("fa-solid")){
+      star.classList.remove("fa-solid");
+      removeFavoriteAlbum(event.target.id)
+   }
+   else{
+      star.classList.add("fa-solid");
+      USER.albumsFav.push(event.target.id);
+      localStorage.setItem('session', JSON.stringify(USER));
+      USUARIOS_REGISTRADOS[USER.usuario] = USER;
+      localStorage.setItem('usuariosRegistrados', JSON.stringify(USUARIOS_REGISTRADOS))
+   }
+}}
 
-     if (document.getElementById(event.target.id)) {
-        var star = document.getElementById(event.target.id);
-        if(star.classList.contains("fa-solid")){
-           star.classList.remove("fa-solid");
-           let index = USER.albumsFav.indexOf(event.target.id);
-           USER.albumsFav.splice( index, 1);
-           localStorage.setItem('session', JSON.stringify(USER));
-           USUARIOS_REGISTRADOS[USER.usuario] = USER;
-           localStorage.setItem('usuariosRegistrados', JSON.stringify(USUARIOS_REGISTRADOS))
-           console.log(JSON.stringify(USER));
-        }
-        else{
-           star.classList.add("fa-solid");
-           USER.albumsFav.push(event.target.id);
-           localStorage.setItem('session', JSON.stringify(USER));
-           USUARIOS_REGISTRADOS[USER.usuario] = USER;
-           localStorage.setItem('usuariosRegistrados', JSON.stringify(USUARIOS_REGISTRADOS))
-           console.log(JSON.stringify(USER));
-        }
-     }
-});
+function removeFavoriteAlbum(targetId){
+      let index = USER.albumsFav.indexOf(targetId);
+      USER.albumsFav.splice( index, 1);
+      localStorage.setItem('session', JSON.stringify(USER));
+      USUARIOS_REGISTRADOS[USER.usuario] = USER;
+      localStorage.setItem('usuariosRegistrados', JSON.stringify(USUARIOS_REGISTRADOS))
+   }
