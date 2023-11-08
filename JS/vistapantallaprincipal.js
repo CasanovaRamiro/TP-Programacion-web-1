65ad1c3f601c636174eabb26b004993d9e86b602
Mariano Sald <odioescribirel@hotmail.com>
2023-11-08 19:16:01 -0300

    undo chang ein vistapantalla principal

--- JS/vistapantallaprincipal.js
+++ JS/vistapantallaprincipal.js
@@ -15,8 +15,8 @@ if(USER.albumsFav.length != 0){
 
 document.addEventListener('mouseover', (event) => {
     //SI EL MOUSE SE POSA SOBRE LA ESTRELLA, SE AGITA POR 1 SEG
-    let star = document.getElementById(event.target.id);
-     if (star != null || star != undefined) {
+     if (document.getElementById(event.target.id)) {
+        var star = document.getElementById(event.target.id);
         if(star.classList.contains("fa-shake")){
            star.classList.remove("fa-shake");
         }
@@ -32,23 +32,81 @@ document.addEventListener('mouseover', (event) => {
 document.addEventListener('click', (event) => {
 
     //AL HACER CLICK EN LA ESTRELLA SE AGREGA AL ARRAY DE FAVORITOS
-    var star = document.getElementById(event.target.id);
-
-     if (star != null || star != undefined) {
-        if(star.classList.contains("fa-solid")){
-           star.classList.remove("fa-solid");
-           let index = USER.albumsFav.indexOf(event.target.id);
-           USER.albumsFav.splice( index, 1);
-           localStorage.setItem('session', JSON.stringify(USER));
-           USUARIOS_REGISTRADOS[USER.usuario] = USER;
-           localStorage.setItem('usuariosRegistrados', JSON.stringify(USUARIOS_REGISTRADOS))
+      const idObjetivo = event.target.id;
+     if (document.getElementById(idObjetivo)) {
+        var listaEstrellas = document.getElementsByClassName('fa-star');
+         let listaEstrellasHermanas = [];
+        for (const st in listaEstrellas) {
+         if (Object.hasOwnProperty.call(listaEstrellas, st)) {
+            const element = listaEstrellas[st];
+            if(element.id.includes(idObjetivo)){
+               listaEstrellasHermanas.push(element);
+            }
+            
+         }
         }
-        else{
-           star.classList.add("fa-solid");
-           USER.albumsFav.push(event.target.id);
-           localStorage.setItem('session', JSON.stringify(USER));
-           USUARIOS_REGISTRADOS[USER.usuario] = USER;
-           localStorage.setItem('usuariosRegistrados', JSON.stringify(USUARIOS_REGISTRADOS))
-        }
+        //SI JUSTO LA ESTRELLA Q DAMOS CLICK ES DE UN DISCO SONANDO
+        if(listaEstrellasHermanas.length > 1){
+         listaEstrellasHermanas.forEach(str => addOrRemoveStar(str, idObjetivo))
+         }
+         //SI NO, SI ES UNA ESTRELLA DE UN DISCO CUALQUIERA
+         else if(!idObjetivo.includes('sonando')){
+            let estrellaDiscoNormal = document.getElementById(idObjetivo);
+            addOrRemoveStar(estrellaDiscoNormal, idObjetivo);
+         }
+         // SI NO PUEDE SER DESDE LA VISTA DE MUSICA SONANDO
+         else{
+            let estrellaId = idObjetivo.slice(8,idObjetivo.length);
+            addOrRemoveStarFromMusicaSonando(listaEstrellasHermanas[0], estrellaId);
+         }
      }
 });
+
+function addOrRemoveStar(estrella, targetId){
+
+   if(estrella.classList.contains("fa-solid"))
+   {
+      estrella.classList.remove("fa-solid");
+      //esto previene que se guarde un valor de disco incorrecto
+      if (!estrella.id.includes('sonando')) {
+         let index = USER.albumsFav.indexOf(targetId);
+         USER.albumsFav.splice( index, 1);
+         localStorage.setItem('session', JSON.stringify(USER));
+         USUARIOS_REGISTRADOS[USER.usuario] = USER;
+         localStorage.setItem('usuariosRegistrados', JSON.stringify(USUARIOS_REGISTRADOS))
+      }
+   }
+   else{
+      estrella.classList.add("fa-solid");
+      if (!estrella.id.includes('sonando')) {
+         USER.albumsFav.push(targetId);
+         localStorage.setItem('session', JSON.stringify(USER));
+         USUARIOS_REGISTRADOS[USER.usuario] = USER;
+         localStorage.setItem('usuariosRegistrados', JSON.stringify(USUARIOS_REGISTRADOS))
+      }
+   }
+}
+function addOrRemoveStarFromMusicaSonando(estrella, targetId){
+
+   if(estrella.classList.contains("fa-solid"))
+   {
+      estrella.classList.remove("fa-solid");
+      //esto previene que se guarde un valor de disco incorrecto
+      if (estrella.id.includes('sonando')) {
+         let index = USER.albumsFav.indexOf(targetId);
+         USER.albumsFav.splice( index, 1);
+         localStorage.setItem('session', JSON.stringify(USER));
+         USUARIOS_REGISTRADOS[USER.usuario] = USER;
+         localStorage.setItem('usuariosRegistrados', JSON.stringify(USUARIOS_REGISTRADOS))
+      }
+   }
+   else{
+      estrella.classList.add("fa-solid");
+      if (estrella.id.includes('sonando')) {
+         USER.albumsFav.push(targetId);
+         localStorage.setItem('session', JSON.stringify(USER));
+         USUARIOS_REGISTRADOS[USER.usuario] = USER;
+         localStorage.setItem('usuariosRegistrados', JSON.stringify(USUARIOS_REGISTRADOS))
+      }
+   }
+}
